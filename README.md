45c26e43b916310626a0b205ec309dbccc3db5da
hydrol0x <mrjacobry@gmail.com>
2022-07-12 19:45:09 -0400

    Configured tailwind

---
+++ README.md
@@ -0,0 +1,3 @@
+# About this site
+
+Site with diabetes informationa and a carb tracker made to practice react and front-end.
---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav>
+      <ul>
+        <a href="/home">Home</a>
+        <a href="/tracker">Carb Tracker</a>
+        <a href="/about">About</a>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
