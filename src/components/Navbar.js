854f568a9612bf35cf3b87def92db2c0ee77dac6
hydrol0x <mrjacobry@gmail.com>
2022-07-13 11:18:22 -0400

    added basic navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,20 @@
 import React from "react";
+import "../navbar.css";
 
 const Navbar = () => {
   return (
-    <nav>
-      <div className="table">
-        <ul className="inline">
-          <li>
-            <a href="/home">Home</a>
-          </li>
-          <li>
-            <a href="/tracker">Carb Tracker</a>
-          </li>
-          <li>
-            <a href="/about">About</a>
-          </li>
-        </ul>
-      </div>
+    <nav className="">
+      <ul className="">
+        <li className="links">
+          <a href="/home">Home</a>
+        </li>
+        <li className="links">
+          <a href="/tracker">Carb Tracker</a>
+        </li>
+        <li className="links">
+          <a href="/about">About</a>
+        </li>
+      </ul>
     </nav>
   );
 };
