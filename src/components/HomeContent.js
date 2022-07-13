d0f2802ee8baf98b037de9040bbcac917526f4fb
hydrol0x <mrjacobry@gmail.com>
2022-07-13 11:33:57 -0400

    added homecontent component

---
+++ src/components/HomeContent.js
@@ -0,0 +1,29 @@
+import "../css/homecontent.css";
+
+const HomeContent = () => {
+  return (
+    <div className="homeContentDiv">
+      <h1 className="mainHeading"> What is Type 1 Diabetes?</h1>
+      <p className="infoPara">
+        {" "}
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+        mollit anim id est laborum.{" "}
+      </p>
+    </div>
+  );
+};
+
+const DiabetesLogo = () => {
+  return (
+    <div className="logoDiv">
+      <img src="img" alt="Diabetes Logo" width="500" height="600"></img>
+    </div>
+  );
+};
+
+export default { HomeContent, DiabetesLogo };
