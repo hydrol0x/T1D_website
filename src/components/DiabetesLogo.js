d566d4f62f77666330cd8ad5a82b83aaec9c9d2c
hydrol0x <mrjacobry@gmail.com>
2022-07-13 11:43:08 -0400

    added logo component

---
+++ src/components/DiabetesLogo.js
@@ -0,0 +1,11 @@
+import diabetesLogoImage from "../t1dlogo.png";
+
+const DiabetesLogo = () => {
+  return (
+    <div className="logoDiv">
+      <img src={diabetesLogoImage} alt="t1dlogo" width="500" height="500" />
+    </div>
+  );
+};
+
+export default DiabetesLogo;
