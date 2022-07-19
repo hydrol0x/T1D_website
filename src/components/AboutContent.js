1e31bea04fe8c4bbd9c0cb96211e1e17180c32fc
hydrol0x <mrjacobry@gmail.com>
2022-07-19 11:39:58 -0400

    About page

---
+++ src/components/AboutContent.js
@@ -0,0 +1,15 @@
+import "../css/aboutContent.css";
+
+const AboutContent = () => {
+  return (
+    <div className="aboutContentDiv">
+      <h1 className="h1About">About</h1>
+      <p className="pAbout">
+        This site is created to help spread some basic information about
+        diabetes and provide a way of tracking carbs for diabetics.
+      </p>
+    </div>
+  );
+};
+
+export default AboutContent;
