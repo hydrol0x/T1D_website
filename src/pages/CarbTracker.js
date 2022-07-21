b5bc491d038fd990587fdf4c0079cb8665d8f812
hydrol0x <mrjacobry@gmail.com>
2022-07-20 20:42:34 -0400

    Fixed file location

---
+++ src/pages/CarbTracker.js
@@ -0,0 +1,10 @@
+import "../css/carbTracker.css";
+
+const CarbTracker = () => {
+  return (
+    <div>
+      <h1 className="trackerh1"> Carb tracker </h1>
+      <div className="tracker">TODO: implement tracker</div>
+    </div>
+  );
+};
