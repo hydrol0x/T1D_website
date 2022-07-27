bce7e998c31786ea9de61acd1be4662ff1094080
hydrol0x <mrjacobry@gmail.com>
2022-07-27 14:24:05 -0400

    Dropdown in carb tracker

--- src/components/FormElement.js
+++ src/components/FormElement.js
@@ -4,7 +4,15 @@ const FormElement = ({ key, id }) => {
   return (
     <div className="FormElement">
       <div className="inputDiv" id="foodDiv">
-        <input id={`${id}food`} type="text" className="food" />
+        <select id={`${id}food`} className="food">
+          {/* TODO: unhardcode values, pass in through json */}
+          <option value="default"> </option>
+          <option value="apple">Apple</option>
+          <option value="rice">Rice</option>
+          <option value="banana">Banana</option>
+          <option value="orange">Orange</option>
+          <option value="bread">Bread</option>
+        </select>
       </div>
       <div className="inputDiv" id="weightDiv">
         <input id={`${id}weight`} type="text" className="weight" />
