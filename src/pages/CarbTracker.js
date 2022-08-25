964269ce4b02d4b5bcd58932d4dbc4c1ae4ab672
hydrol0x <mrjacobry@gmail.com>
2022-08-24 22:02:30 -0400

    Removed useEffect and old functions

--- src/pages/CarbTracker.js
+++ src/pages/CarbTracker.js
@@ -1,23 +1,14 @@
-import { useState } from "react";
 import Tracker from "../components/Tracker";
 
 import "../css/carbTracker.css";
 
 const CarbTracker = () => {
- // Counts how many carbs user has set
-  const [numForms, setFormElements] = useState(5);
+  // Counts how many carbs user has set
 
-  
   return (
     <div className="CarbTrackerDiv">
       <h1 className="trackerh1"> Carb tracker </h1>
-      <h1>{totalCarbs}</h1>
-      <Tracker
-        totalCarbs={totalCarbs}
-        setTotalCarbs={setTotalCarbs}
-        addForm={addForm}
-        numForms={[...Array(numForms).keys()]}
-      />
+      <Tracker />
     </div>
   );
 };
