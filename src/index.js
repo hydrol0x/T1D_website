c2b7aea1d087902a7cc052ff03649fd7c5e9abd5
hydrol0x <mrjacobry@gmail.com>
2022-08-28 17:32:07 -0400

    made sure /home path and / path both display the same elements (i.e display the Home and Home2 elements)

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./css/index.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./css/index.css";
 
 // Import pages to route to
 import Home from "./pages/Home";
+import Home2 from "./pages/Home2";
 import About from "./pages/About";
 import CarbTracker from "./pages/CarbTracker";
 import ErrorPage from "./pages/ErrorPage";
@@ -16,8 +18,22 @@ root.render(
     <Navbar />
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/home" element={<Home />} />
+        <Route
+          path="/"
+          element={
+            <>
+              <Home /> <Home2 />
+            </>
+          }
+        />
+        <Route
+          path="/home"
+          element={
+            <>
+              <Home /> <Home2 />
+            </>
+          }
+        />
         <Route path="/tracker" element={<CarbTracker />} />
         <Route path="/about" element={<About />} />
         <Route path="*" element={<ErrorPage />} />
