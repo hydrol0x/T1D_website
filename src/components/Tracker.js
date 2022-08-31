6041c906869011c78a76c6be1b433197b1c39647
hydrol0x <mrjacobry@gmail.com>
2022-08-30 21:41:07 -0400

    Changed forEach to .map in order to return the elements and render

--- src/components/Tracker.js
+++ src/components/Tracker.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import FormElement from "./FormElement";
 import CreateButton from "./CreateButton";
 import SubmitButton from "./SubmitButton";
@@ -6,32 +7,34 @@ import "../css/tracker.css";
 
 const Tracker = () => {
   const [forms, setForms] = useState({
-    id0: { carbs: 0, amount: 0 },
-    id1: { carbs: 0, amount: 0 },
-    id2: { carbs: 0, amount: 0 },
-    id3: { carbs: 0, amount: 0 },
+    0: { carbs: 0, amount: 0 },
+    1: { carbs: 0, amount: 0 },
+    2: { carbs: 0, amount: 0 },
+    3: { carbs: 0, amount: 0 },
   });
 
   const addForm = () => {
-    setFormElements(numForms + 1);
+    // setForms({...dic.len()+1: { carbs: 0, amount: 0}})
+    // TODO: implement above
   };
 
   return (
     <div className="Tracker">
-      <h1>{totalCarbs}</h1>
       <div className="headingWrapper">
         <h1>Food</h1> <h1>Amount</h1> <h1>Carbs</h1>
       </div>
       <form>
-        {Object.keys(forms).forEach((key, index) => {
-          val = forms[key];
-          <FormElement
-            forms={forms}
-            setForms={setForms}
-            id={key}
-            carbs={val.carbs}
-            amount={val.carbs}
-          />;
+        {Object.keys(forms).map((key, index) => {
+          const val = forms[key];
+          return (
+            <FormElement
+              forms={forms}
+              setForms={setForms}
+              id={key}
+              carbs={val.carbs}
+              amount={val.amount}
+            />
+          );
         })}
       </form>
       {/* <SubmitButton handleSubmit={handleSubmit} /> */}
